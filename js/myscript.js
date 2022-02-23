ac0b548a200e639158ae9465c68231af49ab9384
Busna27 <lucabusnelli2001@gmail.com>
2022-02-23 18:21:35 +0100

    spacing js code

--- js/myscript.js
+++ js/myscript.js
@@ -12,8 +12,10 @@ userBtnGen.addEventListener('click', function() {
 
     document.getElementById('my-name-user').innerHTML = userInputName.value;
     
+
     console.log(`Nome: ${userInputName.value}`);
     console.log(`Distanza KM: ${userInputDistance.value}`);
+
 
     let priceTicket = userInputDistance.value * 0.27;
     console.log(`Prezzo base: ${priceTicket}`);
@@ -24,11 +26,13 @@ userBtnGen.addEventListener('click', function() {
         document.getElementById('my-ticket-type').innerHTML = "Biglietto minorenne";
         console.log('Ha scelto il biglietto minorenne');
     }
+
     if (userInputSelect.value == 2) {
         priceTicket = priceTicket.toFixed(2);
         document.getElementById('my-ticket-type').innerHTML = "Biglietto standard";
         console.log('Ha scelto il biglietto standard');
     }
+
     if (userInputSelect.value == 3) {
         priceTicket -= priceTicket * 0.33
         priceTicket = priceTicket.toFixed(2);
@@ -36,17 +40,16 @@ userBtnGen.addEventListener('click', function() {
         console.log('Ha scelto il biglietto over 65');
     }
     
+
     document.getElementById('my-ticket-price').innerHTML = priceTicket + "<span>&euro;</span>";
     console.log(`Prezzo finale: ${priceTicket}`);
     
 
     let randomInt = Math.floor(Math.random() * 15) + 1;
     document.getElementById('my-ticket-position').innerHTML = randomInt;
-    
+
     let randomIntCode = Math.floor(Math.random() * 99999) + 1;
     document.getElementById('my-ticket-code').innerHTML = randomIntCode;
-    
-    
     
     
     userInputName.value = "";
@@ -60,6 +63,7 @@ const userBtnDelete = document.getElementById('my-btn-delete');
 userBtnDelete.addEventListener('click', function() {
     document.getElementById('my-section-ticket').classList.remove('d-block');
     document.getElementById('my-section-ticket').classList.add('d-none');
+    
     
     userInputName.value = "";
     userInputDistance.value = "";
