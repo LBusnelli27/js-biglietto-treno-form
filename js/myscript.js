310e1c74fd409efcb230a3185c932ff6db98211b
Busna27 <lucabusnelli2001@gmail.com>
2022-02-23 18:10:58 +0100

    all js - done

--- js/myscript.js
+++ js/myscript.js
@@ -8,25 +8,38 @@ const userBtnGen = document.getElementById('my-btn-gen');
 userBtnGen.addEventListener('click', function() {
     document.getElementById('my-section-ticket').classList.remove('d-none');
     document.getElementById('my-section-ticket').classList.add('d-block');
-    document.getElementById('my-name-user').innerHTML = userInputName;
     
 
-    switch (userInputSelect.value) {
-        case 1:
-            console.log('Ha scelto minorenne');
-            break;
-        case 2:
-            console.log('Ha scelto standard');
-            break;
-        case 3:
-            console.log('Ha scelto over 65');
-            break;
+    document.getElementById('my-name-user').innerHTML = userInputName.value;
+    
+    console.log(`Nome: ${userInputName.value}`);
+    console.log(`Distanza KM: ${userInputDistance.value}`);
 
-        default:
-            console.error('Buggino Mannaggino')
+    let priceTicket = userInputDistance.value * 0.27;
+    console.log(`Prezzo base: ${priceTicket}`);
+    
+    if (userInputSelect.value == 1) {
+        priceTicket -= priceTicket * 0.17;
+        priceTicket = priceTicket.toFixed(2);
+        document.getElementById('my-ticket-type').innerHTML = "Biglietto minorenne";
+        console.log('Ha scelto il biglietto minorenne');
     }
-
+    if (userInputSelect.value == 2) {
+        priceTicket = priceTicket.toFixed(2);
+        document.getElementById('my-ticket-type').innerHTML = "Biglietto standard";
+        console.log('Ha scelto il biglietto standard');
+    }
+    if (userInputSelect.value == 3) {
+        priceTicket -= priceTicket * 0.33
+        priceTicket = priceTicket.toFixed(2);
+        document.getElementById('my-ticket-type').innerHTML = "Biglietto over 65";
+        console.log('Ha scelto il biglietto over 65');
+    }
     
+    document.getElementById('my-ticket-price').innerHTML = priceTicket + "<span>&euro;</span>";
+    console.log(`Prezzo finale: ${priceTicket}`);
+    
+
     let randomInt = Math.floor(Math.random() * 15) + 1;
     document.getElementById('my-ticket-position').innerHTML = randomInt;
     
@@ -35,9 +48,6 @@ userBtnGen.addEventListener('click', function() {
     
     
     
-    console.log(userInputName.value);
-    console.log(userInputDistance.value);
-    console.log(userInputSelect.value);
     
     userInputName.value = "";
     userInputDistance.value = "";
